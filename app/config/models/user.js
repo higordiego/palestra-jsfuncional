a84ea5f08a491d752c6de18abd2b5231c1a173cc
Higor Diego <higordiegoti@gmail.com>
2018-01-17 22:13:09 -0300

    subindo alteracões

--- app/config/models/user.js
+++ app/config/models/user.js
@@ -2,7 +2,7 @@ module.exports = ({
     model: {
         name: {type: String, required: true},
         email: {type: String, required: true, index:{unique: true}},
-        passsword: {type: String, required: true},
+        password: {type: String, required: true},
         phone: {type: String, required: true}
     },
     name: 'User',
--- app/modules/_controllers/_user.js
+++ app/modules/_controllers/_user.js
@@ -1,12 +1,13 @@
 module.exports = app => {
-    const User = app.models.User
-    const Persistence = require('../../helpers/persistence')(User)
     const Helpers = require('../../helpers/validate')
+    const User = app.models.User
     return {
         create: (req, res) => {
             const body = {};
             Helpers.validateBody(req.body, 'name', 'email', 'phone', 'password')(body)
-            Persistence.create(res)(body)
+            User.create(body)
+                .then(resp => res.status(201).json(resp))
+                .catch(err => res.status(400).json(err))
         }
     }
 }
