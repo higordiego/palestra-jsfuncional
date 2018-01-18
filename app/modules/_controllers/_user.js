403fe79a644bc60c9f70474b383329d01ee4eab9
Higor Diego <higordiegoti@gmail.com>
2018-01-18 15:08:26 -0300

    subind

--- app/modules/_controllers/_user.js
+++ app/modules/_controllers/_user.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     const Helpers = require('../../helpers/validate')
-    const User = app.models.User
+    const User = require('../_models/user')
     return {
         create: (req, res) => {
             const body = {};
@@ -8,6 +8,23 @@ module.exports = app => {
             User.create(body)
                 .then(resp => res.status(201).json(resp))
                 .catch(err => res.status(400).json(err))
-        }
+        },
+        findAll: (req, res) => 
+            User.find({})
+                .then(resp => res.status(200).json(resp))
+                .catch(err => res.status(400).json(err)),
+        
+        findOne: (req, res) => 
+            User.findOne(req.params)
+                .then(resp => res.status(200).json(resp))
+                .catch(err => res.status(400).json(err)),
+        update: (req, res) =>
+            User.update(req.params, req.body)
+                .then(resp => res.status(200).json(resp))
+                .catch(err => res.status(400).json(err)),
+        delete: (req, res) => 
+            User.remove(req.params)
+                .then(resp => res.status(200).json(resp))
+                .catch(err => res.status(400).json(err)),
     }
 }
--- app/modules/_models/user.js
+++ app/modules/_models/user.js
@@ -1,12 +1,22 @@
-module.exports = app => {
-    const mongoose = require('mongoose')
-    const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-    const user = new Schema({
-        name: {type: String, required: true},
-        email: {type: String, required: true, index:{unique: true}},
-        passsword: {type: String, required: true}
-    })
+const user = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    passsword: {
+        type: String,
+        required: true
+    }
+})
 
-    module.exports = mongoose.model('User', user)
-}
+module.exports = mongoose.model('User', user)
